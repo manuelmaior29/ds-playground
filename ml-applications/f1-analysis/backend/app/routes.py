01cb59d29f3aefbfa8d8f7eb1cd15bdc4265e542
manuelmaior29 <manuelmaior.29@gmail.com>
2023-11-16 21:43:03 +0200

    Removed files

--- ml-applications/f1-analysis/backend/app/routes.py
+++
@@ -1,5 +0,0 @@
-from app import app
-
-@app.route('/')
-def index():
-    return 'Welcome to the F1 Data Visualization Web App!'
--- ml-applications/f1-analysis/backend/config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    SECRET_KEY = 'your_secret_key'
-    # Configure your database settings here
