787217b4e35a8342fb7a2f194542866ee74997dd
manuelmaior29 <manuelmaior.29@gmail.com>
2023-11-18 17:27:24 +0200

    Implemented find closest centroids utility function

---
+++ courses/Unsupervised-Learning-Recommenders-Reinforcement-Learning/utils.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def find_closest_centroids(X, centroids):
+    K = centroids.shape[0]
+    idx = np.zeros(X.shape[0], dtype=int)
+    for ix, x in enumerate(X):
+        closest_centroid = 0
+        min_dist = np.linalg.norm(x - centroids[0])
+        for k in range(K):
+            dist = np.linalg.norm(x - centroids[k])
+            if dist < min_dist:
+                closest_centroid = k
+                min_dist = dist
+        idx[ix] = closest_centroid
+    return idx
+
+def compute_centroids(X, idx, K):
+    pass
+
+def k_means_initialize_centroids(X: np.ndarray, K: int):
+    centroids = []
+    n = len(X)
+    if n > K:
+        indices = np.random.choice(a=list(range(n)), size=(K,), replace=False)
+        centroids = [X[i] for i in indices]
+    return np.array(centroids)
+    
