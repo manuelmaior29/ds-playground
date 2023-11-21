257f339e625173beb9c9ad06c1dda106599ce3ab
manuelmaior29 <manuelmaior.29@gmail.com>
2023-11-21 20:11:09 +0200

    Implemented epsilon optimization

--- courses/Unsupervised-Learning-Recommenders-Reinforcement-Learning/anomaly-detection/utils.py
+++ courses/Unsupervised-Learning-Recommenders-Reinforcement-Learning/anomaly-detection/utils.py
@@ -8,7 +8,17 @@ def load_data():
     with open('data.pkl', 'rb') as f:
         X = pickle.load(f)
         return X
-    return None
+
+def load_example_val():
+    """
+    Load dummy validation preds/gts
+    """
+    y_val, p_val = None, None
+    with open('y_val.pkl', 'rb') as f:
+        y_val = pickle.load(f)
+    with open('p_val.pkl', 'rb') as f:
+        p_val = pickle.load(f)
+    return np.array(y_val), np.array(p_val)
 
 def estimate_gaussian(X):
     """
@@ -35,3 +45,33 @@ def estimate_gaussian(X):
         mu[i] = s1[i] / m
         var[i] = (s2[i] - (s1[i]*s1[i]) / m) / m
     return mu, var
+
+def select_threshold(y_val, p_val): 
+    """
+    Finds the best threshold to use for selecting outliers 
+    based on the results from a validation set (p_val) 
+    and the ground truth (y_val)
+    
+    Args:
+        y_val (ndarray): Ground truth on validation set
+        p_val (ndarray): Results on validation set
+        
+    Returns:
+        epsilon (float): Threshold chosen 
+        F1 (float):      F1 score by choosing epsilon as threshold
+    """
+    best_epsilon = 0.0
+    best_F1 = 0.0
+    F1 = 0.0
+    step_size = (max(p_val) - min(p_val)) / 1000
+    for epsilon in np.arange(min(p_val), max(p_val), step_size):
+        tp = np.sum((y_val == 1) & (p_val <= epsilon))
+        fp = np.sum((y_val == 0) & (p_val <= epsilon))
+        fn = np.sum((y_val == 1) & (p_val > epsilon))
+        prec = tp / (tp + fp)
+        rec = tp / (tp + fn)
+        F1 = (2 * prec * rec) / (prec + rec)
+        if F1 > best_F1:
+            best_F1 = F1
+            best_epsilon = epsilon
+    return best_epsilon, best_F1
