bebdc0ecc2b307b7a7922a9ada224e654cb872f4
manuelmaior29 <manuelmaior.29@gmail.com>
2023-12-01 22:29:16 +0200

    Implemented parameter loading function

--- courses/Unsupervised-Learning-Recommenders-Reinforcement-Learning/recommender-systems/utils.py
+++ courses/Unsupervised-Learning-Recommenders-Reinforcement-Learning/recommender-systems/utils.py
@@ -3,6 +3,23 @@ import tensorflow as tf
 import pandas as pd
 import tqdm
 
+def load_precalc_params(Y_df, num_features):
+    """
+    Args:
+      Y_df (pandas.DataFrame): movie ratings (movieId, movie ratings for userIds)
+    Returns:
+      X (ndarray (num_movies,num_features)): matrix of item features
+      W (ndarray (num_users,num_features)) : matrix of user parameters
+      b (ndarray (1, num_users)            : vector of user parameters (biases)
+      num_movies (int)                    : number of movies
+      num_users (int)                     : number of users
+    """
+    num_movies, num_users = Y_df.shape
+    X = np.random.rand(num_movies, num_features)
+    W = np.random.rand(num_users, num_features)
+    b = np.random.rand(1, num_users)
+    return X, W, b, num_movies, num_users
+
 def load_ratings_small(path, count=1000):
     """
       Args:
@@ -15,9 +32,9 @@ def load_ratings_small(path, count=1000):
     movies_df = pd.read_csv(path + '/movies.csv')
     user_ids = ratings_df['userId'].unique().tolist()
     columns = ['movieId'] + user_ids
-    Y_df, R_df = pd.DataFrame(columns=columns), pd.DataFrame(columns=columns)
+    Y_df = pd.DataFrame(columns=columns, data=np.zeros((movies_df.shape[0], len(columns))))
+    R_df = pd.DataFrame(columns=columns, data=np.zeros((movies_df.shape[0], len(columns))))
     Y_df['movieId'], R_df['movieId'] = movies_df['movieId'], movies_df['movieId']
-    Y_df[user_ids], R_df[user_ids] = 0, 0
     for row in tqdm.tqdm(ratings_df.itertuples()):
         Y_df.loc[row.movieId, row.userId] = row.rating
         R_df.loc[row.movieId, row.userId] = 1
