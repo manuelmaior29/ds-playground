2c65c04729681d09917300804605f212f946b70a
manuelmaior29 <manuelmaior.29@gmail.com>
2024-03-17 14:32:06 +0200

    Implemented transformer encoder

--- ml/general/transformer/model.py
+++ ml/general/transformer/model.py
@@ -46,3 +46,30 @@ class TransformerBlock(nn.Module):
         forward = self.feed_forward(x)
         out = self.dropout(self.norm2(forward + x))
         return out
+
+class Encoder(nn.Module):
+    def __init__(self, vocab_size, embed_size, num_layers, heads, device, forward_expansion, dropout, max_length):
+        super(Encoder, self).__init__()
+        self.embed_size = embed_size
+        self.device = device
+        self.word_embedding = nn.Embedding(vocab_size, embed_size)
+        self.position_embedding = nn.Embedding(max_length, embed_size)
+        self.layers = nn.ModuleList(
+            [
+                TransformerBlock(
+                    embed_size=embed_size,
+                    heads=heads,
+                    dropout=dropout,
+                    forward_expansion=forward_expansion
+                )
+            ]
+        )
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, mask):
+        N, seq_length = x.shape
+        positions = torch.arange(0, seq_length).expand(N, seq_length).to(self.device)
+        out = self.dropout(self.word_embedding(x) + self.position_embedding(positions))
+        for layer in self.layers:
+            out = layer(out, out, out, mask)
+        return out
