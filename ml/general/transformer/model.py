17ba5937f429143d4355a9a00714fedbd2fc77c9
manuelmaior29 <manuelmaior.29@gmail.com>
2024-03-17 14:19:20 +0200

    Implemented transformer block

--- ml/general/transformer/model.py
+++ ml/general/transformer/model.py
@@ -26,3 +26,23 @@ class SelfAttention(nn.Module):
         out = torch.einsum('nhql,nlhd->nqhd', [attention, value_len]).reshape(N, query_len, self.heads * self.head_dim)
         out = self.fc_out(out)
         return out
+    
+class TransformerBlock(nn.Module):
+    def __init__(self, embed_size, heads, dropout, forward_expansion):
+        super(TransformerBlock, self).__init__()
+        self.attention = SelfAttention(embed_size=embed_size, heads=heads)
+        self.norm1 = nn.LayerNorm(embed_size)
+        self.norm2 = nn.LayerNorm(embed_size)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_size, forward_expansion * embed_size),
+            nn.ReLU(),
+            nn.Linear(forward_expansion * embed_size, embed_size)
+        )
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, value, key, query, mask):
+        attention = self.attention(value, key, query, mask)
+        x = self.dropout(self.norm1(attention + query))
+        forward = self.feed_forward(x)
+        out = self.dropout(self.norm2(forward + x))
+        return out
