a4c671c8ee5899d20ae9770b051a99c2c49cf828
manuelmaior29 <manuelmaior.29@gmail.com>
2025-06-27 17:13:28 +0300

    Integrated real search and custom tools in HF project agent

--- ai-agents/huggingface-course-project/agent.py
+++ ai-agents/huggingface-course-project/agent.py
@@ -1,15 +1,25 @@
 from typing import Annotated, List, TypedDict
 
-from langchain_core.messages import AnyMessage, AIMessage, HumanMessage
+from langchain_core.messages import AnyMessage, HumanMessage, SystemMessage
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
-from langgraph.graph import START, END, StateGraph
+from langgraph.graph import StateGraph
 from langchain_ollama import ChatOllama
+from langchain_community.tools import (
+    DuckDuckGoSearchRun
+)
 
-from tools import search_wikipedia_tool, weather_tool
+from tools import (
+    search_wikipedia_tool,
+    text_unreverser_tool
+)
 
-tools = [search_wikipedia_tool, weather_tool]
-llm = ChatOllama(model="llama3-groq-tool-use:8b", verbose=True)
+tools = [
+    search_wikipedia_tool,
+    text_unreverser_tool,
+    DuckDuckGoSearchRun()
+]
+llm = ChatOllama(model="qwen3:8b", verbose=True)
 llm_with_tools = llm.bind_tools(tools)
 
 class AgentState(TypedDict):
@@ -21,12 +31,10 @@ def call_model(state: AgentState) -> dict:
     Invokes the LLM with the current messages and returns its response.
     """
     messages = state["messages"]
-    # print(f"\n--- LLM Input Messages ---")
-    # for msg in messages:
-    #     print(f"  {type(msg).__name__}: {msg.content[:100]}...")
-    #     if isinstance(msg, AIMessage) and msg.tool_calls:
-    #         print(f"    (Tool Calls in history: {msg.tool_calls})")
-    response = llm_with_tools.invoke(messages)
+    sys_msg = SystemMessage(content=f"""You are a helpful assistant that answers to questions directly.
+                            Use any tool available to you to answer the question, but only if it makes sense. 
+                            If you cannot answer the question, answer with "I don't know".""")
+    response = llm_with_tools.invoke([sys_msg] + messages)
     return {"messages": [response]}
 
 # Graph
@@ -44,12 +52,14 @@ agent = workflow.compile()
 if __name__ == "__main__":
     print("Agent started. Type 'exit' to quit.")
     while True:
-        user_input = input("You: ")
+
+        user_input = input("================================== You ==================================\n")
         if user_input.lower() == 'exit':
             break
-
         inputs = {"messages": [HumanMessage(content=user_input)]}
-
-        print("Agent:")
+        
         response = agent.invoke(input=inputs)
-        print(response['messages'][-1].content)
+        # Print the steps
+        for message in response["messages"]:
+            if not isinstance(message, HumanMessage):
+                message.pretty_print()
--- ai-agents/huggingface-course-project/tools.py
+++ ai-agents/huggingface-course-project/tools.py
@@ -1,5 +1,5 @@
-import random
 from langchain.tools import Tool
+from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
 
 def search_wikipedia(query: str) -> str:
     """
@@ -7,21 +7,16 @@ def search_wikipedia(query: str) -> str:
     Useful for factual questions or when you need information on a specific topic.
     """
     try:
-        return f"Simulated Wikipedia search result for '{query}': Information about {query} is vast and covers many aspects, from its historical context to its modern-day applications."
+        result = WikipediaAPIWrapper().run(query)
+        return result
     except Exception as e:
         return f"Error searching Wikipedia for '{query}': {e}"
 
-def get_weather_info(location: str) -> str:
+def get_unreversed_text(text: str) -> str:
     """
-    Fetches dummy weather information for a given location.
+    Unreverses a reversed text.
     """
-    weather_conditions = [
-        {"condition": "Rainy", "temp_c": 15},
-        {"condition": "Windy", "temp_c": 18},
-        {"condition": "Clear", "temp_c": 24},
-    ]
-    data = random.choice(weather_conditions)
-    return f"Weather in {location}: {data['condition']}, {data['temp_c']} deg. Celsius."
+    return text[::-1]
 
 search_wikipedia_tool = Tool(
     name="search_wikipedia",
@@ -29,8 +24,9 @@ search_wikipedia_tool = Tool(
     description="Searches Wikipedia for the given query and returns a summary. Useful for factual questions or when you need information on a specific topic."
 )
 
-weather_tool = Tool(
-    name="get_weather_info",
-    func= get_weather_info,
-    description="Fetches dummy weather information for a given location."
+text_unreverser_tool = Tool(
+    name="text_unreverser",
+    func= get_unreversed_text,
+    description="Unreverses a reversed text."
 )
+
